a72113c9c3f5450976b1d25f5dec027bd64ec95c
ElenaZamyatina <elenzamyatina12@gmail.com>
2022-09-16 08:44:57 +0400

    Задача 21 перерешена

--- ex2/Program.cs
+++ ex2/Program.cs
@@ -1,29 +1,21 @@
 ﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек 
-//и находит расстояние между ними в 3D пространстве.
+//и находит расстояние между ними в 3D пространстве. 
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-int x1 = Coordinate("x", "A");
-int y1 = Coordinate("y", "A");
-int z1 = Coordinate("z", "A");
-int x2 = Coordinate("x", "B");
-int y2 = Coordinate("y", "B");
-int z2 = Coordinate("z", "B");
+Console.Write("Введите координату A: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
 
-int Coordinate(string coordinateName, string dotName)
-{
-    Console.Write($"Введите координату {coordinateName} точки {dotName}: ");
-    return Convert.ToInt16(Console.ReadLine());
-}
+Console.Write("Введите координату B: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
 
-double Decision(double x1, double x2, 
-                double y1, double y2, 
-                double z1, double z2){
-  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
-                   Math.Pow((y2-y1), 2) + 
-                   Math.Pow((z2-z1), 2));
-}
+// формула d_{12}=\sqrt{(x_2-x_1)^2+(y_2-y_1)^2+(z_2-z_1)^2}
+// Pow требует double
+double D = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
 
-double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+Console.WriteLine($"Расстояние между точками  {Math.Round(D,2)}");
 
-Console.WriteLine($"Расстояние между точками  {segmentLength}");
